3143971eb5db6184e0cc9b67f1792ea4d4a92e69
Dominik Vach <vach.dominik@gmail.com>
2020-03-31 00:39:28 +0200

    Pawn movement ahead validation - decorator

--- game_mechanics.py
+++ game_mechanics.py
@@ -1,5 +1,7 @@
 import game_engine
 
+from functools import wraps
+
 def evaluate_click(grids,click):
     grid=grids[0]
     row,col=grid.evaluate_row_column_indices(click)
@@ -7,28 +9,32 @@ def evaluate_click(grids,click):
     return(row,col)
         
 def mark_selected_piece(row,col):
-    for piece in game1.pieces:
-        if piece.row==row and piece.col==col:
-            piece.selected=True
-            print(piece)
+    piece=get_piece_on_tile(row,col)
+    if piece is not None:
+        piece.selected=True
+        print(piece)
         
 def move_or_mark_selected_piece(row,col):
     for piece in game1.pieces:
         if piece.selected==True:
-            piece.row=row
-            piece.col=col
-            piece.selected=False
-            piece.update_piece_label()
+            piece.try_relative_move(row-piece.row,col-piece.col)
             return #either move or mark
     mark_selected_piece(row,col)
             
-
+def get_piece_on_tile(row,col):
+    for piece in game1.pieces:
+        if piece.row==row and piece.col==col:
+            return(piece)
+    return(None)
 
 ##################### CHESS Game Mechanics ########################
     
-
-
-
+def validate_move(func):
+    def function_wrapper(x):
+        print("Before calling " + func.__name__)
+        func(x)
+        print("After calling " + func.__name__)
+    return function_wrapper
 
 class Piece:
     def __init__(self,row,col,owner):
@@ -48,8 +54,7 @@ class Piece:
    
     def update_piece_label(self):
         self.piece_label.position=[3+POSITION_X+self.col*(CELL_SIZE_X+MARGIN),30+POSITION_Y+self.row*(CELL_SIZE_Y+MARGIN)]
-        
-        
+
     def move(self,steps,direction): #direction = 1 ahead then clockwise to 8        
         if direction in [8,1,2]:
             self.row-=steps
@@ -59,9 +64,57 @@ class Piece:
             self.col+=steps
         elif direction in [6,7,8]:
             self.col-=steps
+            
+    def adjust_validation(f):
+        @wraps(f)
+        def function_wrapper(instance,row,col):
+            return(f(instance,row,col))
+        return(function_wrapper)  
+    
+    @adjust_validation
+    def validate_relative_move(self,row,col):
+        valid=True
+        return(valid)
+        
+    def try_relative_move(self,row,col):
+        if self.validate_relative_move(row,col):    
+            self.relative_move(row,col)
+            self.update_piece_label()
+        self.selected=False
+           
+    def relative_move(self,row,col):
+        self.row+=row
+        self.col+=col
    
 class Pawn(Piece):
-    pass
+    def adjust_validation(f):
+        @wraps(f)
+        def function_wrapper(instance,row,col):
+            if row==-1 and col==0:    
+                return(f(instance,row,col))
+            else:
+                return(False)
+        return(function_wrapper)
+    
+    @adjust_validation
+    def validate_relative_move(self,row,col):
+        print(row,col)
+        valid=True
+        return(valid)
+
+    def try_relative_move(self,row,col):
+        print(self.validate_relative_move(row,col))
+        if self.validate_relative_move(row,col):    
+            self.relative_move(row,col)
+            self.update_piece_label()
+        self.selected=False
+        
+    #def adjust_validation(self,function):
+    #    def function_wrapper(row,col):
+    #        print("Validated before")
+    #        function(row,col)
+    #        print("Validated after")
+    #    return(function_wrapper)
 
 class Rook(Piece):
     pass
@@ -95,6 +148,16 @@ class Game:
         self.pieces=[]
         for i in range(8):
             self.pieces.append(Pawn(6,i,1))
+        for i in [0,7]:
+            self.pieces.append(Rook(7,i,1))
+        for i in [1,6]:
+            self.pieces.append(Knight(7,i,1))
+        for i in [2,5]:
+            self.pieces.append(Bishop(7,i,1))
+        for i in [3] :
+            self.pieces.append(Queen(7,i,1))
+        for i in [4] :
+            self.pieces.append(King(7,i,1))
                           
 game1=Game()
                         

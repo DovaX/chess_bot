85c4084d2a93999702ae965b02454818cde3c078
Dominik Vach <vach.dominik@gmail.com>
2020-04-13 23:12:08 +0200

    strategy

---
+++ chess_strategy.py
@@ -0,0 +1,13 @@
+
+
+#change_in_pieces_dict={"King":-1}
+
+class Objective:
+    def __init__(self,change_in_pieces_dict):
+        
+
+
+class Hypothesis:
+    def __init__(self,pieces,conclusion):
+        pass
+    
